6f55d902f6f80458313a56cac6bd498d470ca681
Panita Aiamsamarng <panitabeem@gmail.com>
2023-11-04 02:28:45 +0700

    last update

--- src/pages/preference/PreferenceFriend.js
+++ src/pages/preference/PreferenceFriend.js
@@ -10,14 +10,27 @@ import { useEffect, useState } from 'react'
 const PreferenceFriend = () => {
   const { friend_id } = useParams()
   const [media, setMedia] = useState([])
+  const [friendUsername, setFriendUsername] = useState('')
   const [mediaPreference, setMediaPreference] = useState([])
   const [errorOnce, setErrorOnce] = useState(false)
   const isLogin = useSelector((state) => state.isLogin.isLogin)
   const navigate = useNavigate()
+
   useEffect(()=>{
     !isLogin && navigate('/')
   }, [isLogin, navigate])
   
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    axios.post(`${process.env.REACT_APP_API}/get-friend/`, {friend_id}, {
+      headers: {
+      'Authorization': `Bearer ${token}`
+    }})
+    .then((response) => {
+      setFriendUsername(response.data.result[0].username)
+    })
+  }, [friend_id])
+
   useEffect(() => {
     const token = localStorage.getItem('token')
     axios.get(`${process.env.REACT_APP_API}/preference-friend/${friend_id}`, {
@@ -56,7 +69,7 @@ const PreferenceFriend = () => {
     <MetaHeader title={`รายการโปรด`} />
     <Navigation />
     <div className='container mx-auto p-10'>
-      <h1 className='text-4xl mt-10 mb-5'>รายการโปรด ของ {friend_id}</h1>
+      <h1 className='text-4xl mt-10 mb-5'>รายการโปรด ของ {friendUsername}</h1>
       <div className='grid place-content-center grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-2.5 sm:gap-5 md:gap-7'>
         {media.length > 0 && !errorOnce && media.map((media) => <MediaCard key={media.id} media={media} mediaPreference={mediaPreference} setMediaPreference={setMediaPreference}/>)}
       </div>
